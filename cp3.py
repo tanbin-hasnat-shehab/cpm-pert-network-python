7c5c785a2e191f9f1427e7cafd88cdc014131b91
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-09-22 14:01:06 +0600

    Update cp3.py

--- cp3.py
+++ cp3.py
@@ -7,11 +7,11 @@ t1=st.text_input(f'{1}')
 t2=st.text_input(f'{2}')
 if st.button('ok'):
     sheet.cell(row=1,column=1).value=t1
-    wb.save('/tmp/data.xlsx')
+    wb.save('tmp/data.xlsx')
     
-    f = open("tmp/demofile.txt", "w")
+    f = open("/tmp/demofile.txt", "w")
     f.writelines([t1,'\n',t2])
     f.close()
-    f = open("tmp/demofile.txt", "r")
+    f = open("/tmp/demofile.txt", "r")
     st.write(f.read())
     

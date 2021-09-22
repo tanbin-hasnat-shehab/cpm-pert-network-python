d11cecf0d5bf4bc4fefb66f7f5bf9377035047b6
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-09-22 11:12:53 +0600

    Update cp3.py

--- cp3.py
+++ cp3.py
@@ -2,17 +2,16 @@ from openpyxl import load_workbook
 import streamlit as st
 
 
-
+wb=load_workbook('data.xlsx')
+sheet=wb.active
 
 input_t=st.text_input('here')
 if st.button('add'):
-	wb=load_workbook('data.xlsx')
-	sheet=wb.active
+	
 	a=len(sheet['A'])
 	sheet.cell(row=a+1, column=1).value=input_t
-	wb.save('data.xlsx')
+	
 if st.button('ok show'):
-	wb=load_workbook('data.xlsx')
-	sheet=wb.active
+	
 	for i in range(len(sheet['A'])):
 		st.write(sheet.cell(row=i+1,column=1).value)

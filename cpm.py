f50c7253b49ff9b9c635bba194f3b216ba6a1d81
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-09-22 22:56:06 +0600

    Add files via upload

---
+++ cpm.py
@@ -0,0 +1,111 @@
+import networkx as nx   
+import matplotlib.pyplot as plt
+import matplotlib
+import random
+import string
+
+
+def graph(mytask,days,precedors,result_tasks,xx):
+	class points:
+		def __init__(self,name,duration,dependency,already):
+			self.name=name
+			self.duration=duration
+			self.dependency=dependency
+			self.already=already
+	task=[]
+	for i in range(len(mytask)):
+		frame=points(mytask[i], days[i], precedors[i],False)
+		task.append(frame)
+	letters = string.ascii_lowercase
+	G = nx.DiGraph()
+	all_nodes=[]
+	for i in range(200):
+		all_nodes.append((i))
+	c=1
+	aa=random.randint(0,1000)
+	def common_data(list1, list2):
+	    result = False
+	    for x in list1:
+	        for y in list2:
+	            if x == y:
+	                result = True
+	                return result               
+	    return result
+	for i in range(len(task)):
+		if task[i].dependency[0]=='-':
+			task[i].left_node=all_nodes[0]
+		else:
+			for j in range(len(task)):
+				if i==j:
+					pass
+				else:
+					if common_data(task[i].dependency, task[j].dependency):
+						task[i].left_node=all_nodes[c]
+						task[j].left_node=all_nodes[c]
+		c+=1
+
+	for i in range(len(task)):
+		if hasattr(task[i], 'left_node'):
+			pass
+		else:
+			task[i].left_node=all_nodes[c]
+
+		c+=1
+
+	for i in range(len(task)):
+		
+		for j in range(len(task[i].dependency)):
+			for k in range(len(task)):
+				if  task[i].dependency[j]==task[k].name:
+					if hasattr(task[k], 'right_node'):
+						pass
+					else:
+						task[k].right_node=task[i].left_node
+
+
+	for i in range(len(task)):
+		if hasattr(task[i], 'right_node'):
+			pass
+		else:
+			task[i].right_node=all_nodes[c]
+	#########################################################################################
+	G = nx.DiGraph()
+	ss=10
+	labels={}
+	for i in range(len(task)):
+		ax=int(result_tasks[f'task{i}']['ES'])
+		bx=int(result_tasks[f'task{i}']['EF'])
+		cx=int(result_tasks[f'task{i}']['LS'])
+		dx=int(result_tasks[f'task{i}']['LF'])
+		
+		if (ax==cx) and (bx==dx):
+			dec=True
+			print(dec)
+			G.add_edge(task[i].left_node,task[i].right_node)		
+		else:
+			dec=False
+			print(dec)
+			G.add_edge(task[i].left_node,task[i].right_node)
+		labels[(task[i].left_node,task[i].right_node)]=f'{ax},{bx}\n{task[i].name}\n{cx},{dx}'
+
+	#edge_colors = ['r' if dec==True else 'b' for e in G.edges]
+	#edge_color=edge_colors
+	pos=nx.spring_layout(G)
+	nx.draw_networkx_edges(G, pos,arrowsize=8)
+
+
+
+	
+	#, arrowstyle='->', arrowsize=10
+	nx.draw(G, pos,node_size =200,with_labels = True)
+	nx.draw_networkx_edge_labels(G,pos,edge_labels=labels,font_size=xx)
+
+
+	#plt.style.use('default')
+	#plt.subplots(figsize=(20,20))
+	plt.savefig('a.png')
+	#plt.show()
+	z=('a.png')
+	
+	return z
+

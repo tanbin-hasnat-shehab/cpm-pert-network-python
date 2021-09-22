57d701b61c93ffbd5e119cf85247bbdec8f5aeb9
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-09-22 14:03:03 +0600

    Update cp3.py

--- cp3.py
+++ cp3.py
@@ -1,13 +1,11 @@
 import streamlit as st
-from openpyxl import load_workbook
 
-wb=load_workbook('tmp/data.xlsx')
-sheet=wb.active
+
+
 t1=st.text_input(f'{1}')
 t2=st.text_input(f'{2}')
 if st.button('ok'):
-    sheet.cell(row=1,column=1).value=t1
-    wb.save('tmp/data.xlsx')
+    
     
     f = open("/tmp/demofile.txt", "w")
     f.writelines([t1,'\n',t2])

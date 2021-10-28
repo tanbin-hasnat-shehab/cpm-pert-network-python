2612d6f2a13f757025b03f98cbe166eaeaf05f1f
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-10-28 18:27:13 +0600

    Update cpm.py

--- cpm.py
+++ cpm.py
@@ -5,7 +5,7 @@ import random
 import string
 
 
-def graph(mytask,days,precedors,result_tasks,xx,myweight):
+def graph(mytask,days,precedors,result_tasks,xx,myweight,f_size):
 	class points:
 		def __init__(self,name,duration,dependency,already):
 			self.name=name
@@ -89,6 +89,7 @@ def graph(mytask,days,precedors,result_tasks,xx,myweight):
 		labels[(aa*task[i].left_node,aa*task[i].right_node)]=f'{ax},{bx}\n{task[i].name}\n{cx},{dx}'
 	weights = nx.get_edge_attributes(G,'weight').values()
 	colors = nx.get_edge_attributes(G,'color').values()
+	plt.figure(1,figsize=(f_size,f_size))
 	pos=nx.spring_layout(G)
 	nx.draw_networkx_edges(G, pos,width=list(weights),edge_color=colors,arrowstyle='->')
 
@@ -97,16 +98,19 @@ def graph(mytask,days,precedors,result_tasks,xx,myweight):
 	
 	#, arrowstyle='->', arrowsize=10
 	
-	nx.draw(G, pos,node_size =200,with_labels = True)
+	nx.draw(G, pos,node_size =20*f_size,with_labels = True)
 	nx.draw_networkx_edge_labels(G,pos,edge_labels=labels,font_size=xx)
+	print(f'thisssssssssssssssssss isssssssssssssssss {G.size()}')
 
 
 	#plt.style.use('default')
 	#plt.figure(1,figsize=(5,5))
 	try:
+		
 		plt.savefig('/tmp/a.png')
 		z=('/tmp/a.png')
 	except:
+		
 		plt.savefig('tmp/a.png')
 		z=('tmp/a.png')
 

07759646333916902004c12846749c12b74a646d
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2021-10-28 16:29:58 +0600

    Update cp3.py

--- cp3.py
+++ cp3.py
@@ -5,6 +5,7 @@ import streamlit as st
 st.set_page_config(layout="wide")
 n=st.slider('number of tasks', min_value=1, max_value=100, value=5, step=1)
 text_on_graph=st.slider('text_on_graph', min_value=1, max_value=20, value=6, step=1)
+myweight=st.slider('critical path line weight', min_value=1, max_value=5, value=3, step=1)
 c1, c2,c3 = st.columns((1,1,5))
 if n:
     with c1:
@@ -89,7 +90,7 @@ if st.button('ok'):
         mydic=calculation.show_results('tmp/cpm.txt')
     tasks=mydic
     #print(mydic)
-    img=cpm.graph(names, duration_one_d, dep, mydic, text_on_graph)
+    img=cpm.graph(names, duration_one_d, dep, mydic, text_on_graph,myweight)
 
 
 
